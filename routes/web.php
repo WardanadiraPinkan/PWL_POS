7eb77b251fba5339bdbb98c89702da6427aa0dbf
Wardanadira Pinkan <wardanadira04@gmail.com>
2024-09-12 13:05:16 +0700

    TUGAS 3 - PRAKTIKUM 6

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,25 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/level' ,[LevelController::class, "index"] );
+Route::get('/kategori', [KategoriController::class, "index"]);
+Route::get('/user', [UserController::class, "index"]);
