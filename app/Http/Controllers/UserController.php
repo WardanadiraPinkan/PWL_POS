aa089bf75efff6998003da238a874ad45de7f6d2
Wardanadira Pinkan <wardanadira04@gmail.com>
2024-09-18 20:13:51 +0700

    TUGAS 4 - PRAKTIKUM 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,9 @@ class UserController extends Controller
 {
     public function index()
     {
-       $user = UserModel::where('level_id', 1)->first();
+       $user = UserModel::findOr(20, ['username', 'nama'], function (){
+            abort(404);
+       });
        return view('user', ['data' => $user]);
     }
 }
