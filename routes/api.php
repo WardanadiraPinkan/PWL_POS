43e55e61418babc247675638a9928483f4ba40a3
Wardanadira Pinkan <wardanadira04@gmail.com>
2024-11-06 09:20:22 +0700

    TUGAS 9 API 1 - PRAKTIKUM 2

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,11 @@ use App\Http\Controllers\Api\RegisterController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request){
+    return $request->user();
+});
